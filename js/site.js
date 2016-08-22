a4c5584c1fc86876f85a85203e86f8e213677e0c
Pranav Joshi <Pranav Joshi>
2016-08-22 21:56:51 +1000

    rename and move files

--- js/site.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function()
-{
-    // target counter jquery plugin
-    $("[data-plugin-targetcounter]").TargetCounter({
-        failureMsg : "You need ${0} more in brokerage to qualify"  // custom failure message
-    });
-});
